33258ca67b8c6facfe3c7f11a66fb76958605f95
Chinemerem Daniel <danpresh302@gmail.com>
2025-04-04 23:46:57 +0100

    added fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chinemerem Daniel</title>
     @vite('resources/css/app.css')
-    @vite('resources/css/app.js')
+    @vite('resources/js/app.js')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
